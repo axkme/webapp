3ecb0c5a76020f1b0eb6078d4e29cc5da66473b8
TuGG <tugg.solo@gmail.com>
2016-05-02 14:02:36 +0700

    add debug tool

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,7 @@
 ﻿'use strict';
 var module = angular.module('app', ['ngCookies']);
 module.config(function ($httpProvider) {
+    Raven.config('https://5e81acabdff64627a6e8f2bf354700c5@app.getsentry.com/76464').install();
     $httpProvider.interceptors.push('httpRequestInterceptor');
 });
 var app = {
